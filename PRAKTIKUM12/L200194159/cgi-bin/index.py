e3f8e8ede78bc44b116094af2915c8ff0fc7ded5
L200194159 <55585316+L200194159@users.noreply.github.com>
2019-12-19 15:15:40 +0700

    Add files via upload

---
+++ PRAKTIKUM12/L200194159/cgi-bin/index.py
@@ -0,0 +1,10 @@
+print("<!DOCTYPE html>\n")
+print("""<html>
+<head><title>Akses dengan SimpleHTTPRequestHadler</title></head>
+<body><h3>Data Diri Mahasiswa</h3>""")
+print("""<table><tr><td>Nama</td><td>:</td><td>Juniar Darma Yati</td></tr>
+		<tr><td>Alamat tinggal</td><td>:</td><td>Surakarta</td></tr>
+		<tr><td>Tempat, tanggal lahir</td><td>:</td><td>Buluh Rampai, 14 Juni 2001</td></tr>
+		<tr><td>Tempat wisata favorit</td><td>:</td><td>Pantai</td></tr>
+		<tr><td>Motto</td><td>:</td><td>Manusia adalah makhluk sosial yang saling membutuhkan</td></tr></table>""")
+print("</body></html>")
